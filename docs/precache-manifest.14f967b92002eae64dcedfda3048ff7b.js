eae526b5fe3b9dd4433668526f7e5c642b3dc97c
damiantruant <truantdamian@gmail.com>
2020-11-05 17:08:14 -0300

    publish prod

---
+++ docs/precache-manifest.14f967b92002eae64dcedfda3048ff7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f05975515f23bd0b90ab1207c826ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7271d6fc23843681a4c",
+    "url": "/static/css/main.24c1fc60.chunk.css"
+  },
+  {
+    "revision": "df85a477b54a5dbf9248",
+    "url": "/static/js/2.c9926598.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.c9926598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7271d6fc23843681a4c",
+    "url": "/static/js/main.f26a8048.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  }
+]);
